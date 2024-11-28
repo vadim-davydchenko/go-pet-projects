d0668e66c97b49d4b00eba66aa55913f0897d5c9
Вадим Давыдченко <vdavidchenko@mobiledep.ru>
2024-11-28 19:17:30 +0300

    add automigrate

---
+++ shortLinks/migrations/auto.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+	"shortLinks/link"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&link.Link{})
+}
