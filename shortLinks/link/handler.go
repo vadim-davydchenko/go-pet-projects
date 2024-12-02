af742ed6592f57ec75e3bed6064b7d996cebcad5
Вадим Давыдченко <vdavidchenko@mobiledep.ru>
2024-12-02 19:11:11 +0300

    add update link, delete link and check exist link

--- shortLinks/link/handler.go
+++ shortLinks/link/handler.go
@@ -1,10 +1,12 @@
 package link
 
 import (
-	"fmt"
 	"net/http"
 	"shortLinks/pkg/req"
 	"shortLinks/pkg/res"
+	"strconv"
+
+	"gorm.io/gorm"
 )
 
 type LinkHandlerDeps struct {
@@ -42,6 +44,7 @@ func (handler *LinkHandler) Create() http.HandlerFunc {
 		createdLink, err := handler.LinkRepository.Create(link)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
 		}
 		res.Json(w, createdLink, 201)
 	}
@@ -49,13 +52,50 @@ func (handler *LinkHandler) Create() http.HandlerFunc {
 
 func (handler *LinkHandler) Update() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		body, err := req.HandleBody[LinkUpdateRequest](&w, r)
+		if err != nil {
+			return
+		}
+		idString := r.PathValue("id")
+		id, err := strconv.ParseUint(idString, 10, 32)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		link, err := handler.LinkRepository.Update(&Link{
+			Model: gorm.Model{
+				ID: uint(id),
+			},
+			Url:  body.Url,
+			Hash: body.Hash,
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		res.Json(w, link, 200)
 	}
 }
 
 func (handler *LinkHandler) Delete() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		fmt.Println(id)
+		idString := r.PathValue("id")
+		id, err := strconv.ParseUint(idString, 10, 32)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		_, err = handler.LinkRepository.GetbyId(uint(id))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+		err = handler.LinkRepository.Delete(uint(id))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		res.Json(w, nil, 200)
 	}
 }
 
--- shortLinks/link/repository.go
+++ shortLinks/link/repository.go
@@ -2,6 +2,8 @@ package link
 
 import (
 	"shortLinks/pkg/db"
+
+	"gorm.io/gorm/clause"
 )
 
 type LinkRepository struct {
@@ -25,6 +27,31 @@ func (repo *LinkRepository) Create(link *Link) (*Link, error) {
 func (repo *LinkRepository) GetByHash(hash string) (*Link, error) {
 	var link Link
 	result := repo.Database.DB.First(&link, "hash = ?", hash)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &link, nil
+}
+
+func (repo *LinkRepository) Update(link *Link) (*Link, error) {
+	result := repo.Database.DB.Clauses(clause.Returning{}).Updates(link)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return link, nil
+}
+
+func (repo *LinkRepository) Delete(id uint) error {
+	result := repo.Database.DB.Delete(&Link{}, id)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (repo *LinkRepository) GetbyId(id uint) (*Link, error) {
+	var link Link
+	result := repo.Database.DB.First(&link, id)
 	if result.Error != nil {
 		return nil, result.Error
 	}
