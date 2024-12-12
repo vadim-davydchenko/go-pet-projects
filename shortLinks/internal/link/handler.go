2ed5bd7b6a861a2ba9d6653db597df019cbfc823
Вадим Давыдченко <vdavidchenko@mobiledep.ru>
2024-12-12 11:29:30 +0300

    read and write form context, add check unauthed

--- shortLinks/internal/link/handler.go
+++ shortLinks/internal/link/handler.go
@@ -1,6 +1,7 @@
 package link
 
 import (
+	"fmt"
 	"net/http"
 	"shortLinks/configs"
 	"shortLinks/pkg/middleware"
@@ -55,6 +56,10 @@ func (handler *LinkHandler) Create() http.HandlerFunc {
 
 func (handler *LinkHandler) Update() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		email, ok := r.Context().Value(middleware.ContextEmailKey).(string)
+		if ok {
+			fmt.Println(email)
+		}
 		body, err := req.HandleBody[LinkUpdateRequest](&w, r)
 		if err != nil {
 			return
--- shortLinks/pkg/middleware/auth.go
+++ shortLinks/pkg/middleware/auth.go
@@ -1,20 +1,38 @@
 package middleware
 
 import (
-	"fmt"
+	"context"
 	"net/http"
 	"shortLinks/configs"
 	"shortLinks/pkg/jwt"
 	"strings"
 )
 
+type key string
+
+const (
+	ContextEmailKey key = "ContextEmailKey"
+)
+
+func writeUnauthed(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write([]byte(http.StatusText(http.StatusUnauthorized)))
+}
 func IsAuthed(next http.Handler, config *configs.Config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authedHeader := r.Header.Get("Authorization")
+		if !strings.HasPrefix(authedHeader, "Bearer ") {
+			writeUnauthed(w)
+			return
+		}
 		token := strings.TrimPrefix(authedHeader, "Bearer ")
 		isValid, data := jwt.NewJWT(config.Auth.Secret).Parse(token)
-		fmt.Println(isValid)
-		fmt.Println(data)
-		next.ServeHTTP(w, r)
+		if !isValid {
+			writeUnauthed(w)
+			return
+		}
+		ctx := context.WithValue(r.Context(), ContextEmailKey, data.Email)
+		req := r.WithContext(ctx)
+		next.ServeHTTP(w, req)
 	})
 }
